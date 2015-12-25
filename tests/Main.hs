dd2b4ea4b7b9271850e1c8bf7b515fa8011f4bc1
mrkkrp <markkarpov@opmbx.org>
2015-12-25 16:24:42 +0600

    add some tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -31,9 +31,60 @@
 -- ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 -- POSSIBILITY OF SUCH DAMAGE.
 
+{-# OPTIONS -fno-warn-orphans #-}
+
 module Main (main) where
 
-import Test.Framework (defaultMain)
+import Control.Monad ((>=>))
+import Data.Char (isAlphaNum)
+import Data.Maybe (isJust)
+import Data.Text (Text)
+import Test.Framework
+import Test.Framework.Providers.HUnit (testCase)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+import Test.HUnit hiding (Test, path)
+import Test.QuickCheck
+import Web.PathPieces
+import Web.Slug
+import qualified Data.Text as T
 
 main :: IO ()
-main = defaultMain []
+main = defaultMain [tests]
+
+tests :: Test
+tests = testGroup "Slug properties"
+  [ testProperty "Slug of slug is the slug" prop_changeStops
+  , testProperty "Alpha-numeric chars are necessary and sufficient"
+    prop_needAlphaNum
+  , testProperty "Path pieces accept correct slugs" prop_pathPiece
+  , testCase "Removal of apostrophe" prop_apostropheRemoval
+  , testCase "Path pieces ignore case of slugs" prop_pathPieceCase
+  ]
+
+instance Arbitrary Text where
+  arbitrary = T.pack <$> arbitrary
+
+prop_changeStops :: Text -> Property
+prop_changeStops x = v (f x) === v (g x)
+  where f = mkSlug
+        g = mkSlug >=> mkSlug . unSlug
+        v = either (Left . show) Right
+
+prop_needAlphaNum :: Text -> Property
+prop_needAlphaNum x = hasAlphaNum x ==> isJust (mkSlug x)
+  where hasAlphaNum = isJust . T.find isAlphaNum
+
+prop_pathPiece :: Text -> Property
+prop_pathPiece x =
+  case mkSlug x of
+    Nothing -> property True
+    Just slug -> fromPathPiece (unSlug slug) === Just slug
+
+prop_apostropheRemoval :: Assertion
+prop_apostropheRemoval = unSlug <$> mkSlug text @?= Just slug
+  where text = "That's what I thought about doin' that stuff!"
+        slug = "thats-what-i-thought-about-doin-that-stuff"
+
+prop_pathPieceCase :: Assertion
+prop_pathPieceCase = fromPathPiece text @?= mkSlug text
+  where text = "thiS-Строка-hAs-коМбиНацию-of-DiffErent-Cases" :: Text
